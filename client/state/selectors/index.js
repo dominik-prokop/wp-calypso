988a4968a416c50dc5997ecf09685faeb4c55359
Marin Atanasov <tyxla@abv.bg>
2017-04-20 00:53:24 +0300

    State: Rename core update status selector

--- client/state/selectors/index.js
+++ client/state/selectors/index.js
@@ -147,6 +147,6 @@ export isTwoStepSmsEnabled from './is-two-step-sms-enabled';
 export isUpdatingJetpackSettings from './is-updating-jetpack-settings';
 export isUserRegistrationDaysWithinRange from './is-user-registration-days-within-range';
 export isUserSettingUnsaved from './is-user-setting-unsaved';
-export isWordpressUpdateSuccesful from './is-wordpress-update-successful';
+export isWordpressUpdateSuccessful from './is-wordpress-update-successful';
 export shouldCloseVideoEditorModal from './should-close-video-editor-modal';
 export shouldShowVideoEditorError from './should-show-video-editor-error';
